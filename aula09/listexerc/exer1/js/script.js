e5a3c7957752b54f7db662523ed1747273daf586
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-09-04 15:23:37 -0400

    js_comm02

--- aula09/listexerc/exer1/js/script.js
+++ aula09/listexerc/exer1/js/script.js
@@ -1,9 +1,15 @@
 /* Soma de Números Pares: Escreva um programa que calcule a soma de todos os números pares de 1 a N, onde N é um número fornecido pelo usuário. */
 
 var num;
+var soma_pares = 0;
 
 function soma_par() {
   num = document.getElementById("number").value;
 
-  console.log(soma);
+  for (var i = 0; i <= num; i++) {
+    if (i % 2 == 0) {
+      soma_pares = i + soma_pares;
+    }
+  }
+  console.log("A soma dos números pares de 1 a " + num + " é: " + soma_pares);
 }
