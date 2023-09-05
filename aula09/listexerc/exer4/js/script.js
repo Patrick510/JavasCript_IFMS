eb30fbd7bacdbdcbf4d5c349cd7c4252544e1ebd
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-09-05 17:05:12 -0400

    js_comm09

--- aula09/listexerc/exer4/js/script.js
+++ aula09/listexerc/exer4/js/script.js
@@ -1,1 +1,22 @@
 // Fatorial: Crie um programa que calcule o fatorial de um número fornecido pelo usuário
+var num;
+var fatorial;
+
+function envia() {
+  num = document.getElementById("number").value;
+
+  if (num <= 1) {
+    console.log("valor", i);
+    return 1;
+  } else {
+    fatorial = 1;
+    for (i = 2; i <= num; i++) {
+      fatorial *= i;
+      console.log(fatorial); // 120
+    }
+  }
+
+  var paragrafo = document.createElement("p");
+  paragrafo.innerHTML = `O fatorial de ${num}: ${fatorial}`;
+  document.body.appendChild(paragrafo);
+}
