a6efa2ca3799b17050de77512bad5dd35e59e0ee
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-10-18 15:19:02 -0400

    jscomm25

---
+++ aula16/js/script.js
@@ -0,0 +1,37 @@
+const search = document.querySelector('.search-box button')
+const container = document.querySelector('.container')
+const weatherBox = document.querySelector('.weather-box')
+const weatherDetail = document.querySelector('.weather-details')
+const error404 = document.querySelector('.not-found')
+
+
+search.addEventListener('click', ()=>{
+    console.log('carregar dados');
+
+    const APIKey = '6798be00aca7b5ad944f1bcb1bb4b30b'
+    const city = document.querySelector('.search-box input').value;
+
+    if(city === '') return;
+
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+    )
+
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json)
+        if(json.cod === '404'){
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetail.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');
+            return;
+        }
+
+        //switch(){
+        //    case json.cod
+        //}
+    }) 
+
+})
